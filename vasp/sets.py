a6f02c379fdbdb22a9c4dc056e57ab3794af4112
Zhenbin Wang <z9wang@eng.ucsd.edu>
2017-08-24 15:50:29 -0700

    minor fix

--- vasp/sets.py
+++ vasp/sets.py
@@ -1,15 +1,46 @@
-from __future__ import unicode_literals, division
+from __future__ import print_function, division
+from pymatgen.io.vasp.sets import MPRelaxSet
+from pymatgen.io.vasp import Kpoints
+import math
 
-__author__ = 'wzb, ywk'
+__author__ = 'wzb'
 __email__ = 'z9wang at ucsd.edu'
 __date__ = '03/20/17'
 
 
-from pymatgen.io.vasp.sets import MPStaticSet, MPNonSCFSet, MPHSEBSSet, \
-    MPHSERelaxSet, MVLElasticSet, DictSet, VaspInputSet
-from pymatgen.core import Structure
-from pymatgen.io.vasp import Kpoints
-import os
-from monty.serialization import loadfn
+class SOFCVaspInputSet(MPRelaxSet):
+    """
+    for sofcs calc.
+    """
 
-MODULE_DIR = os.path.dirname(os.path.abspath(__file__))
+    def __init__(self, structure, min_length=10, **kwargs):
+        user_incar_settings = kwargs.get("user_incar_settings", {})
+        defaults = {"EDIFF": 1e-4,
+                    "EDIFFG": -0.03,
+                    "ISIF": 2,
+                    "NELMIN": 5,
+                    "ISMEAR": 0,
+                    "LCHARG": "False",
+                    "NSW": 500}
+        defaults.update(user_incar_settings)
+        kwargs["user_incar_settings"] = defaults
+        super(SOFCVaspInputSet, self).__init__(structure, **kwargs)
+        self.min_length = min_length
+
+    @property
+    def kpoints(self):
+
+        """
+        create k-points for surface/interface structure relaxation.
+        """
+
+        kpt = super(SOFCVaspInputSet, self).kpoints
+        kpt.comment = "Automatic mesh"
+        lengths = self.structure.lattice.abc
+        kpt_calc = [max(1, int(round(math.ceil(self.min_length / x)))) for x in lengths]
+        if kpt_calc == [1, 1, 1]:
+            kpt.style = Kpoints.supported_modes.Gamma
+            kpt.kpts[0] = kpt_calc
+        else:
+            kpt.kpts[0] = kpt_calc
+        return kpt
